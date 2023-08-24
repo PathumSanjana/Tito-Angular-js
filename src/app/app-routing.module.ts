022cdaa847c48753c35e4a5bac72c4079096f658
PathumSanjana <silvakp-se19030@stu.kln.ac.lk>
2023-08-24 23:52:34 +0530

    Routes added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContentComponent } from './content/content.component';
+import { JavaComponent } from './java/java.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+ 
+  {
+    path:'',
+    component:ContentComponent
+  },
+  {
+    path:'java',
+    component:JavaComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
